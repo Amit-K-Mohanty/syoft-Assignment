664dd74d834a48bd2cd1f28381cc15a0462b0cbf
Amit Mohanty <68189793+Amit-K-Mohanty@users.noreply.github.com>
2023-07-12 12:29:34 +0530

    Update auth.guard.ts

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -9,10 +9,19 @@ import { AppData } from "./app-date.service";
 export class AuthGuard implements CanActivate {
     constructor(private router: Router) { }
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        if (AppData.isAuthenticated) {
+        if (state.url === '/dashboard' && AppData.isAuthenticated) {
             return true
-        } else {
-            this.router.navigate(['welcome'])
+        }
+        if (state.url === '/login' && !AppData.isAuthenticated) {
+            // this.router.navigate(['/login'])
+            return true
+        }
+        if (state.url === '/login' && AppData.isAuthenticated) {
+            this.router.navigate(['/dashboard'])
+            return true
+        }
+        else {
+            this.router.navigate(['/welcome'])
             return false
         }
 
